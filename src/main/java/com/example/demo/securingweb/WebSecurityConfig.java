d647c520d54566e0c9f99fbbc75288857cd086bb
biata <biataru2@gmail.com>
2023-04-04 17:36:44 +0300

    security and users

--- src/main/java/com/example/demo/securingweb/WebSecurityConfig.java
+++ src/main/java/com/example/demo/securingweb/WebSecurityConfig.java
@@ -1,48 +1,47 @@
 package com.example.demo.securingweb;
 
+import com.example.demo.services.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+   private final CustomUserDetailsService userDetailsService;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http
+               .authorizeRequests()
+               .antMatchers("/")
+               .permitAll()
+               .anyReguest.authenticated()
+               .and()
+               .formLogin()
+               .loginPage("/login");
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/", "/Main","/SignUp","/resources/**",  "../../static/Main.css").permitAll()
-//                        .anyRequest().authenticated()
-//                );
-//        http.authorizeHttpRequests().requestMatchers("/login*", "/", "/Main", "/SignUp", "/resources/**",
-//                        "/resources/static/**", "/resources/templates/**", "/static/Login.css", "/templates/**",
-//                        "../../static/Main.css", "/Login.css" ).permitAll().and()
-//                .formLogin((form) -> form
-//                        .loginPage("/Login")
-//                        .permitAll()
-//                )
-//                .logout((logout) -> logout.permitAll());
-
-        http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
-        return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("biataru@gmail.com")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
+    @Bean
+public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(8);
+}
+
 }
---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,27 @@
+//package com.example.demo.services;
+//
+//import com.example.demo.entity.User;
+//import com.example.demo.entity.enums.Role;
+//import com.example.demo.repo.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@Slf4j
+//@RequiredArgsConstructor
+//public class UserService {
+//    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    public boolean createUser (User user){
+//        String email = user.getEmail();
+//        if (userRepository.findByEmail(user.getEmail()) != null ) return false;
+//        user.setActive(true);
+//        user.getPassword(passwordEncoder.encode(user.getPassword()));
+//        user.getRoles().add(Role.ROLE_USER);
+//        log.info("Saving new User with email: {}", email);
+//        return true;
+//    }
+//}
