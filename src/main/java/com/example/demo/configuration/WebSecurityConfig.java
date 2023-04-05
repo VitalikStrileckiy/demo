2f0105c56cc08273a5ffdcb2277cfdf54dfbf286
Oleh Pashkevych <Oleh.Pashkevych@tomtom.com>
2023-04-05 16:03:19 +0300

    Basic authentication is implemented.

---
+++ src/main/java/com/example/demo/configuration/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.example.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+//                .usersByUsernameQuery("select username, password, enabled "
+//                        + "from users "
+//                        + "where username = ?")
+//                .authoritiesByUsernameQuery("select username, authority "
+//                        + "from authorities "
+//                        + "where username = ?")
+        ;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/Main","/SignUp","/*.css", "/img/*"
+                                , "/login*", "/logout*", "/error")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic()
+//                .formLogin()
+//                .loginPage("/login.html")
+//                .defaultSuccessUrl("/", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+;
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,22 +1,16 @@
 package com.example.demo.controllers;
 
-import com.example.demo.dto.AppointmentDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main(Model model){
+    public String main(Model model) {
         model.addAttribute("");
         return "Main";
-    }
-    @GetMapping("/Login")
-    public String login(Model model){
-        return "../html/Login";
     }
 
     @GetMapping("/SignUp")
@@ -28,7 +22,5 @@ public class MainController {
     public String userPage(Model model){
         return "UserPage";
     }
-
-
 
 }
